172318f602cbeba0195c41ee7009953f83b49e7b
N01dor <81870840+Nold0r@users.noreply.github.com>
2024-03-31 19:00:11 +0700

    add models thaumonomicon

--- src/main/java/net/noldor/thaumcraft/Thaumcraft.java
+++ src/main/java/net/noldor/thaumcraft/Thaumcraft.java
@@ -2,6 +2,7 @@ package net.noldor.thaumcraft;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.noldor.thaumcraft.items.ModeItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +13,6 @@ public class Thaumcraft implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-
-		LOGGER.info("Hello Fabric world!");
+		ModeItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/noldor/thaumcraft/items/ModeItems.java
@@ -0,0 +1,24 @@
+package net.noldor.thaumcraft.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.noldor.thaumcraft.Thaumcraft;
+
+public class ModeItems {
+    public static final Item BOOK = registerItem("book", new Item(new FabricItemSettings()));
+
+
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(Thaumcraft.MODID, name ), item);
+    }
+    public static void registerModItems(){
+        Thaumcraft.LOGGER.info("Registering Mod Item for"+ Thaumcraft.MODID);
+
+    }
+}
